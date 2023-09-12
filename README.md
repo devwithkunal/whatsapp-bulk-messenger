07fa8c97322a856ec58e3fca7f9651de3ffd8892
Kunal <Kunalkhan370@gmail.com>
2023-09-12 19:45:54 +0530

    New Version With Some Fixes

--- README.md
+++ README.md
@@ -16,12 +16,14 @@ Write the message in the `message.txt` file.
 ### 3. Create Numbers List
 Add numbers in the `numbers.txt` file. Add each number in a new line. There is no limit to numbers.
 
-### 4. Change Configs (optional)
+### 4. Change Configs
 Change configs on lines `11` to `14` in the `main.py` file
 - `login_time` - Time for login (in seconds)
 - `new_msg_time` - Time for a new message (in seconds)
 - `send_msg_time` - Time for sending a message (in seconds)
 - `country_code` - Set your country code
+- `action_time` -  Set time for button click action
+- `image_path` - Absolute path to you image (Set `None` for text only.)
 
 ### 5. Run
 Run the `main.py` file.
--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from urllib.parse import quote
+from selenium.webdriver.common.by import By
 import time
 
 # Config
-login_time = 30     # Time for login (in seconds)
-new_msg_time = 5    # TTime for a new message (in seconds)
-send_msg_time = 5   # Time for sending a message (in seconds)
-country_code = 91   # Set your country code
+login_time = 30                 # Time for login (in seconds)
+new_msg_time = 5                # TTime for a new message (in seconds)
+send_msg_time = 5               # Time for sending a message (in seconds)
+country_code = 91               # Set your country code
+action_time = 2                 # Set time for button click action
+image_path = 'image.png'        # Absolute path to you image
 
 # Create driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Encode Message Text
 with open('message.txt', 'r') as file:
-    msg = quote(file.read())
+    msg = file.read()
 
 # Open browser with default link
 link = 'https://web.whatsapp.com'
@@ -29,10 +31,24 @@ time.sleep(login_time)
 with open('numbers.txt', 'r') as file:
     for n in file.readlines():
         num = n.rstrip()
-        link = f'https://web.whatsapp.com/send/?phone={country_code}{num}&text={msg}'
+        link = f'https://web.whatsapp.com/send/?phone={country_code}{num}'
         driver.get(link)
         time.sleep(new_msg_time)
+        # Click on button to load the input DOM
+        if(image_path):
+            attach_btn = driver.find_element(By.CSS_SELECTOR, '._1OT67')
+            attach_btn.click()
+            time.sleep(action_time)
+            # Find and send image path to input
+            msg_input = driver.find_elements(By.CSS_SELECTOR, '._2UNQo input')[1]
+            msg_input.send_keys(image_path)
+            time.sleep(action_time)
+        # Start the action chain to write the message
         actions = ActionChains(driver)
+        for line in msg.split('\n'):
+            actions.send_keys(line)
+            # SHIFT + ENTER to create next line
+            actions.key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
         actions.send_keys(Keys.ENTER)
         actions.perform()
         time.sleep(send_msg_time)
